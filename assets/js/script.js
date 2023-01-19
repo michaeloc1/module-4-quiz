8d2c86a1c8c22e6a1d5f1ab4160d7e9248b31ea7
michaeloc1 <michaeloc@verizon.net>
2023-01-19 12:06:54 -0500

    appended start to webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ var timeInterval;
 var body = document.body;
 body.style.fontFamily = "cursive"
 body.style.color = "royalblue"
+var sectionEl = document.createElement("section");
 appendHeader();
+appendStartPage();
 //*****************make header**************************** */
  function appendHeader(){
    // var setTimer = 0;
@@ -26,6 +28,55 @@ appendHeader();
 
 }
 //******************************************************** */
+
+//**************append section****************************** */
+body.appendChild(sectionEl);
+//******************************************************** */
+
+//******************************************************** */
+
+//**************append start page***************************** */
+function appendStartPage(){
+    var startH1El = document.createElement("h1");
+    var startPEl = document.createElement("p");
+    var startBtnEl = document.createElement("button");
+    startBtnEl.setAttribute("class", "format-btn");
+    formatButton(startBtnEl);
+    startH1El.textContent = "Take the JavaScript Quiz";
+    startPEl.textContent ="This is a timed Quiz";
+    startBtnEl.textContent = "Start Quiz";
+    startH1El.style.textAlign = "center";
+    startPEl.style.textAlign = "center";
+    sectionEl.appendChild(startH1El);
+    sectionEl.appendChild(startPEl);
+    sectionEl.appendChild(startBtnEl);
+    startBtnEl.addEventListener("click", function(){
+        console.log("click of start quiz")
+    })
+    
+
+}
+
+//************************************************************ */
+
+function formatButton(btn){
+    btn.style.backgroundColor = "royalblue";
+    btn.style.color = "white";
+    btn.style.padding = "20px";
+    btn.style.display = "block";
+    btn.style.margin = "0 auto";
+    btn.style.borderRadius = "10px";
+    btn.addEventListener("mouseover", function(){
+    btn.style.backgroundColor = "red";
+    btn.style.cursor = "pointer";
+    
+    })
+    btn.addEventListener("mouseout", function(){
+        btn.style.backgroundColor = "royalblue";
+        
+        })
+}
+
 
 //*******************make high score***************************//
 function appendHighScores(){
