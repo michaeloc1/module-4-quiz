2e8d85e9984a72d16cd469bb503ead8f8005217a
michaeloc1 <michaeloc@verizon.net>
2023-01-20 11:55:13 -0500

    added questions and getQuestions and checkAnswer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
 var setTimer = 0;
 var timeInterval;
 var body = document.body;
+var correct = 0;
 //body.style.fontFamily = "cursive"
 //body.style.color = "royalblue"
 //var sectionEl = document.createElement("section");
+var questions = [{
+    prompt:"test question",
+    option1: "value1",
+    option2: "value2",
+    option3:  "value3",
+    option4: "value4",
+    correctOption: 3
+},
+{
+    prompt: "test question 2",
+    option1: "value 1 for question2",
+    option2: "value 2 for question2",
+    option3:  "value3 for question2",
+    option4: "value4 for question2",
+    correctOption: 4
+
+}
+
+
+]
 appendHeader();
 appendStartPage();
 //*****************make header**************************** */
@@ -96,42 +117,88 @@ function appendQuiz(){
     body.appendChild(quizSectionEl);
     quizSectionEl.appendChild(quizH1El);
     quizSectionEl.appendChild(quizDivContainer)
+    var numMaker = 0
     for (let i = 0; i < 4; i++){
         var quizDivEl = document.createElement("div");
         quizDivEl.setAttribute("class","quiz-answers");
-        quizDivEl.setAttribute("number", i);
+        quizDivEl.setAttribute("number", ++numMaker);
         //formatAnswers();
         quizDivContainer.appendChild(quizDivEl);
         //formatAnswers();
-
     }
-    function formatAnswers(){
-    var quizAnwerClass = document.querySelector(".quiz-answers");
-    //console.log(quizAnwerClass);
-   // quizAnwerClass.style.backgroundColor = "royalblue";
-   // quizAnwerClass.style.color = "white";
-   // quizAnwerClass.style.padding = "20px";
-   //  quizAnwerClass.style.borderRadius = "10px";
-   // quizAnwerClass.style.width = "100px"
-    quizAnwerClass.addEventListener("mouseover", function(){
-    quizAnwerClass.style.backgroundColor = "red";
-    quizAnwerClass.style.cursor = "pointer";
-    quizAnwerClass.margin = "10px";
-   
-    
-    })
-    quizAnwerClass.addEventListener("mouseout", function(){
-        quizAnwerClass.style.backgroundColor = "royalblue";
-        
-        })
+    var divEls = document.querySelectorAll(".quiz-answers");
+     //divEls[1].onclick = function(){console.log(this.getAttribute("number"))} 
+     console.log(divEls);
+     for(var i = 0; i < 4; i++){
+        var indEl = divEls[i]
+         divEls[i].addEventListener("mouseover", function(event){
+            console.log("Mouse over box")
+           // indEl.style.backgroundColor = "red";
+          var currentDiv = event.currentTarget;
+           currentDiv.style.backgroundColor = "red";
+           currentDiv.style.cursor = "pointer";
+         })
 
-    }
-    
+         divEls[i].addEventListener("mouseout", function(event){
+            console.log("Mouse over box")
+           // indEl.style.backgroundColor = "red";
+          var currentDiv = event.currentTarget;
+           currentDiv.style.backgroundColor = "royalblue";
+           currentDiv.style.cursor = "pointer";
+         })
 
+         divEls[i].addEventListener("click", function(event){
+            console.log("You clicked me");
+            var currentDiv = event.currentTarget;
+            var answerNum = currentDiv.getAttribute("number");
+            //answerNum++;
+            checkAnswer(answerNum);
+            getQuestions(quizH1El,divEls); 
+         })
+     }
 
-    
+     getQuestions(quizH1El,divEls);        
+     
+
 }
 
+function getQuestions(quizQuestionEl,quizAnswersEl){
+
+    //var newArr = questions.shift();
+
+   // console.log(newArr)
+   // console.log(newArr.prompt)
+   if(questions.length > 0){
+        var newArr = questions.shift();
+        quizQuestionEl.textContent = newArr.prompt;
+        quizAnswersEl[0].textContent = newArr.option1;
+        quizAnswersEl[1].textContent = newArr.option2;
+        quizAnswersEl[2].textContent = newArr.option3;
+        quizAnswersEl[3].textContent = newArr.option4;
+        correct = newArr.correctOption;
+   }
+   else{
+    //console.log(correct)
+
+       // clearInterval(timeInterval);
+       // setTimer = 0;
+       // clearQuiz();
+   }
+  
+}
+
+//************check answer*****************************
+function checkAnswer(num){
+    console.log("Im in the checkAnswer function")
+    if(correct == num){
+        //console.log("Correct")
+        alert("correct")
+    }
+    else{
+        alert("wrong")
+    }
+}
+//************************************************************/
 
 //*******************make high score***************************//
 function appendHighScores(){
