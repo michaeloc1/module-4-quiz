08a121cc20ff5e02fbb88dd9918261f2aa30f79c
michaeloc1 <michaeloc@verizon.net>
2023-01-21 10:15:43 -0500

    added high score functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,47 @@ var setTimer = 0;
 var timeInterval;
 var body = document.body;
 var correct = 0;
+gotRight = 0;
+var scores = [];
+if(localStorage.getItem("scores") != null){
+    scores = JSON.parse(localStorage.getItem("scores"));
+}
 //body.style.fontFamily = "cursive"
 //body.style.color = "royalblue"
 //var sectionEl = document.createElement("section");
 var questions = [{
-    prompt:"test question",
-    option1: "value1",
-    option2: "value2",
-    option3:  "value3",
-    option4: "value4",
+    prompt:"What does the ! symbol stand for in JavaScript?",
+    option1: "and",
+    option2: "or",
+    option3:  "not",
+    option4: "than",
     correctOption: 3
 },
 {
-    prompt: "test question 2",
-    option1: "value 1 for question2",
-    option2: "value 2 for question2",
-    option3:  "value3 for question2",
-    option4: "value4 for question2",
+    prompt: "The index of arrays start at the value",
+    option1: "-1",
+    option2: "1",
+    option3:  "3",
+    option4: "0",
     correctOption: 4
+
+},
+{
+    prompt: "What is the correct way to remove the last element from an array and return that element",
+    option1: "array.pop()",
+    option2: "array.reduce()",
+    option3:  "array.push()",
+    option4: "array.shift()",
+    correctOption: 1
+
+},
+{
+    prompt: "Which is not the proper way you can declare a variable",
+    option1: "let",
+    option2: "var",
+    option3:  "make",
+    option4: "const",
+    correctOption: 3
 
 }
 
@@ -72,7 +95,7 @@ function appendStartPage(){
     startBtnEl.setAttribute("class", "format-btn");
     formatButton(startBtnEl);
     startH1El.textContent = "Take the JavaScript Quiz";
-    startPEl.textContent ="This is a timed Quiz";
+    startPEl.textContent ="This is a timed Quiz. You will be given 30 seconds to complete.  If you get a question wrong 5 seconds will be deducted from the time remaining.";
     startBtnEl.textContent = "Start Quiz";
     //startH1El.style.textAlign = "center";
     //startPEl.style.textAlign = "center";
@@ -119,11 +142,16 @@ function appendQuiz(){
     var quizSectionEl = document.createElement("section");
     var quizH1El = document.createElement("h1");
     var quizDivContainer = document.createElement("div");
+    //make element to hold correct or wrong
+    var quizH2El = document.createElement("h2");
+    //********************************************* */
     quizH1El.setAttribute("class", "quiz-question");
     quizDivContainer.setAttribute("class", "container");
     body.appendChild(quizSectionEl);
     quizSectionEl.appendChild(quizH1El);
     quizSectionEl.appendChild(quizDivContainer)
+  
+
     var numMaker = 0
     for (let i = 0; i < 4; i++){
         var quizDivEl = document.createElement("div");
@@ -133,6 +161,12 @@ function appendQuiz(){
         quizDivContainer.appendChild(quizDivEl);
         //formatAnswers();
     }
+    //append the element to bottom of divs
+    quizDivContainer.appendChild(quizH2El);
+    //quizH2El.textContent = "Correct";
+
+    //**************************************************** */
+
     var divEls = document.querySelectorAll(".quiz-answers");
      //divEls[1].onclick = function(){console.log(this.getAttribute("number"))} 
     // console.log(divEls);
@@ -200,13 +234,30 @@ function getQuestions(quizQuestionEl,quizAnswersEl){
 
 //************check answer*****************************
 function checkAnswer(num){
+    var getH2El = document.querySelector("h2")
+    
+   // alert(getH2El)
     console.log("Im in the checkAnswer function")
     if(correct == num){
         //console.log("Correct")
-        alert("correct")
+        getH2El.textContent = "Correct"
+        gotRight++;
     }
     else{
-        alert("wrong")
+        if(setTimer < 5){
+            setTimer = 0;
+            spanEl.textContent = setTimer;
+            clearInterval(timeInterval);
+            deleteHTML();
+            appendFinish();
+        }
+        else{
+            setTimer = setTimer - 5
+            getH2El.textContent = "Wrong"
+        }
+
+
+
     }
 }
 //************************************************************/
@@ -229,11 +280,107 @@ function starTimer(){
 //********************************************************** */
 
 function appendFinish(){
-    alert("Quiz finished");
+    var h1El = document.createElement("h1");
+    var btn = document.createElement("button");
+    var sectionEl = document.createElement("section");
+    var h3El = document.createElement("H3");
+    var textBox = document.createElement("INPUT");
+    var divContainerEl = document.createElement("div");
+    var divCenterEl = document.createElement("div")
+    divContainerEl.setAttribute("class", "container2")
+    divCenterEl.setAttribute("class", "center")
+    textBox.setAttribute("type", "text");
+    textBox.setAttribute("maxlength", "3");
+    textBox.setAttribute("class", "format-textbox")
+    btn.setAttribute("class", "format-btn");
+    h3El.setAttribute("class", "format-h3-finish")
+    formatButton(btn)
+    body.appendChild(sectionEl);
+    sectionEl.appendChild(divContainerEl);
+    divContainerEl.appendChild(divCenterEl);
+
+    divCenterEl.appendChild(h1El);
+    divCenterEl.appendChild(h3El);
+    divCenterEl.appendChild(textBox);
+    divCenterEl.appendChild(btn);
+    h1El.textContent = "Quiz Over";
+    h3El.textContent = "You got " + gotRight + " out of 4 correct";
+     btn.textContent = "Add Score";
+         btn.addEventListener("click", function(){
+        var initials = textBox.value;
+
+    if(scores.length === 0){
+        scores[0] = {
+            init: initials,
+            scr: gotRight
+        }
+    }
+    else{
+        scores.push(
+            {
+                init: initials,
+                scr: gotRight
+            }
+        )
+    }
+   localStorage.setItem("scores", JSON.stringify(scores));
+
+
+       // console.log("textbox " + initials);
+       // localStorage.setItem("initials", initials)
+       // localStorage.setItem("score", gotRight)
+        deleteHTML();
+        appendHighScores()
+    
+    });
+   // btn.addEventListener("click", function(){
+        //deleteHTML1();
+      //  appendQuiz();
+
+   // });
 }
 
 //*******************make high score***************************//
 function appendHighScores(){
+    var HighScoreSectionEl = document.createElement("section")
+    var HighScoreH1El = document.createElement("h1");
+    var HighScoreH4El = document.createElement("h4");
+    var HighScorebtnEl1 = document.createElement("button");
+    var HighScorebtnEl2 = document.createElement("button");
+    var HighScoreElBr   = document.createElement("br")
+    HighScorebtnEl1.setAttribute("class", "format-btn");
+    HighScorebtnEl2.setAttribute("class", "format-btn");
+     body.appendChild(HighScoreSectionEl);
+    HighScoreSectionEl.appendChild(HighScoreH1El);
+    HighScoreH1El.textContent = "High Scores";
+    for (let i = 0; i < 5; i++){
+        var HighScoreH4El = document.createElement("h4")
+        HighScoreH4El.setAttribute("class","high-scores");
+        //formatAnswers();
+        HighScoreSectionEl.appendChild(HighScoreH4El);
+        //formatAnswers();
+    }
+    HighScorebtnEl1.textContent = "Clear High Scores";
+    HighScorebtnEl2.textContent = "Start Again";
+    formatButton(HighScorebtnEl1);
+    formatButton(HighScorebtnEl2);
+    HighScoreSectionEl.appendChild(HighScorebtnEl1);
+    HighScoreSectionEl.appendChild(HighScoreElBr);
+    HighScoreSectionEl.appendChild(HighScorebtnEl2);
+
+    var HighScoreH4Els = document.querySelectorAll("h4");
+   // HighScoreH4Els[0].textContent = "MOC: " + "4"
+   // HighScoreH4Els[1].textContent = "abc: " + "2"
+   var newArr = JSON.parse(localStorage.getItem("scores"))
+   newArr.reverse();
+   for(i = 0; i < 5; i++){
+    HighScoreH4Els[i].textContent = newArr[i].init + ": " + newArr[i].scr;
+   }
+   HighScorebtnEl2.addEventListener("click", function(){
+    deleteHTML();
+    appendStartPage();
+   })
+
     
 }
 //******************************************************** */
